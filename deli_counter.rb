493216fb6d415293ad24515052e807a05fc8f728
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-19 21:23:07 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+john 
+array = []
+def line(array)
+  array = []
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  end
+end 
+
+def take_a_number(array,name)
+  array =  []
+  array.count do |line_number|
+     if array >= 1
+       puts "Welcome, #{name}. You are number #{line_number + 1}."
+     end 
+  end 
+end 
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+
+=begin
+def now_serving(array)
+=end
